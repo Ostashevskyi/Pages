b04981543fbeb0c54d8c56f945cb3a5f4a823098
Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-02-27 23:40:33 +0200

    created a burger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+function toogleBurger() {
+    document.querySelector('.header__burger-btn').addEventListener('click', () => {
+        document.querySelector('.header__wrapper').classList.toggle('open');
+    })
+}
+
+toogleBurger();
